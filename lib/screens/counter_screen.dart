2559152770d24206503365cabf78b39e0e3a8d0a
wirlowsky <wirlowsky@gmail.com>
2022-10-24 18:09:24 +0200

    Finalizado el counter App

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -12,11 +12,30 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int contador = 10;
+  void increase( int action){
+    print(contador);
+  switch(action){
+    case 0:
+      contador++;
+      break;
+    case 1:
+      contador --;
+      break;
+    case 2:
+      contador = 0;
+      break;
+  }
+
+    setState(() {
+
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
     const fonStyle = TextStyle(color: Colors.red, fontSize: 30);
+    print(CustomFloatingActions(increase));
 
     return Scaffold(
       appBar: AppBar(
@@ -34,42 +53,43 @@ class _CounterScreenState extends State<CounterScreen> {
             Text(contador.toString(), style: fonStyle)
           ])),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Row(
 
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              contador++;
-              print(contador.toString());
-
-              setState(() {});
-            },
-            child: const Icon(Icons.add),
-          ),
-          //const SizedBox(width: 20,),
-          FloatingActionButton(
-            onPressed: () {
-              contador--;
-              print(contador.toString());
-
-              setState(() {});
-            },
-
-            child: const Icon(Icons.exposure_minus_1),
-          ),
-         // const SizedBox(width: 20,),
-          FloatingActionButton(
-            onPressed: () {
-              contador=0;
-              print(contador.toString());
-
-              setState(() {});
-            },
-            child: const Icon(Icons.reset_tv),
-          )
-        ]
-      ),
+      floatingActionButton: CustomFloatingActions(increase)
     );
   }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+
+final Function increaseFn;
+
+
+const CustomFloatingActions(this.increaseFn, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+ return Row(
+
+
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          onPressed: () => increaseFn(0),
+          child: const Icon(Icons.add),
+        ),
+        //const SizedBox(width: 20,),
+        FloatingActionButton(
+          onPressed: () => increaseFn(1),
+
+          child: const Icon(Icons.exposure_minus_1),
+        ),
+        // const SizedBox(width: 20,),
+        FloatingActionButton(
+          onPressed: () => increaseFn(2),
+          child: const Icon(Icons.reset_tv),
+        )
+      ]
+  );
+  }
+
 }
